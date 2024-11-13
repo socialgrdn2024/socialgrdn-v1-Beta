ee5dcbeefcaa4e9bb59d4a0d1310314ab3ba0c20
Shelyn Del Mundo <socialgrdn2024@gmail.com>
2024-11-12 18:43:35 -0700

    COVERALL

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-// // jest.config.js
-// module.exports = {
-//     transform: {
-//       "^.+\\.(js|jsx)$": "babel-jest",
-//     },
-//     transformIgnorePatterns: ["<rootDir>/node_modules/"],
-//     setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
-//   };
-  
